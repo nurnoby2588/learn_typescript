36525ccc2eb43f3935a20478e0071d6a9213fcd9
nurnoby admin <nurnoby323@gmail.com>
2025-05-21 11:04:40 +0600

    array type  define

---
+++ user-define-type/array_type.ts
@@ -0,0 +1,13 @@
+// how to define array single data type
+// define: 1
+// let names:string[];
+// define: 2
+let names:Array<string>;
+names=["nurnoby","nasim"];
+console.log(names[0]);
+
+// how to define array multiple data type
+
+let multipleTypeNames: (string | number)[]
+multipleTypeNames=[2588,"nurnoby"];
+console.log(multipleTypeNames[0]);
